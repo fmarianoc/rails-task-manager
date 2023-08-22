83544a65d9c3031da658147d608c9824d55ea231
Filipe Mariano <filipemarianoc@gmail.com>
2023-08-22 16:50:46 -0300

    before bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # verb 'path', to: 'controller#action'
+
+  # see all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # create a task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # see one task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
